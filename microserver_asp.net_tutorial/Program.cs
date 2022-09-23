b8996d0a8b3339ed84b1cd4f45aea118bb1e9926
san27sin <sin27san@gmail.com>
2022-09-23 14:03:50 +0300

    Add project files.

---
+++ microserver_asp.net_tutorial/Program.cs
@@ -0,0 +1,54 @@
+namespace microserver_asp.net_tutorial
+{
+    
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+
+
+            /*ѕри добавление в майн классов - они автоматически станов€тс€ сервесами.
+             * ћы можем сюда добавл€ть в конфигурацию свои сервесы.
+             * ¬се типы которые мы регистрируем - называютс€ сервесами.
+             */
+
+            // Add services to the container.
+
+
+            //ѕервое, что мы должны выучить, обращаемс€ к базовому свойству сервеса, помогает нам зарегистрировать вспомагательные сервисы
+            //в рамках нашего приложени€ мы можем регистрировать различные сервесы
+
+            //ћожем указывать врем€ жизни сервера
+
+            /* онтроллеры нужны дл€ 
+             * ѕапка controllers - эстетически создана дл€ хранени€ всех классов контроллеров
+             */
+
+
+            builder.Services.AddControllers();
+            // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+            builder.Services.AddEndpointsApiExplorer();
+            builder.Services.AddSwaggerGen();
+
+            var app = builder.Build();
+
+            // Configure the HTTP request pipeline.
+            if (app.Environment.IsDevelopment())
+            {
+                app.UseSwagger();
+                app.UseSwaggerUI();
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseAuthorization();
+
+
+            app.MapControllers();
+
+            app.Run();
+        }
+    }
+}
